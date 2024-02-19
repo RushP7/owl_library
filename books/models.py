7adad5ee54f3579f09f6364969c75128f06772c7
RushP7 <rushilxo5@gmail.com>
2024-02-19 16:58:59 -0500

    type field corrected char length

--- books/models.py
+++ books/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     owl_id = models.CharField(max_length=255, unique=True)
-    book_type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    book_type = models.CharField(max_length=2, choices=TYPE_CHOICES)
 
     def __str__(self):
         return str(self.title)
