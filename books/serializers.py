15b9128b20752d3ec097a6e96fe8f4539bfb477f
RushP7 <rushilxo5@gmail.com>
2024-02-19 18:29:30 -0500

    docstrings

--- books/serializers.py
+++ books/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Book model.
+
+    Converts Book model instances into JSON representations.
+    """
+
     class Meta:
         model = Book
         fields = ['title', 'author', 'owl_id', 'book_type']
--- books/views.py
+++ books/views.py
@@ -3,5 +3,11 @@ from .models import Book
 from .serializers import BookSerializer
 
 class BookListAPIView(generics.ListAPIView):
+    """
+    API endpoint to retrieve a list of all books in the library.
+
+    Inherits from ListAPIView provided by Django REST Framework.
+    """
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
