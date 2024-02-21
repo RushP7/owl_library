0a5e79049767dc7eb91e627d3d438a54a95d810b
RushP7 <rushilxo5@gmail.com>
2024-02-21 16:09:49 -0500

    view for borrow

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from django.utils import timezone
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 
@@ -27,3 +29,27 @@ class BookListByAuthor(generics.ListAPIView):
         """
         author = self.kwargs['author']
         return Book.objects.filter(author=author)
+    
+
+class BorrowBook(generics.UpdateAPIView):
+    """
+    API endpoint to borrow a book.
+
+    Inherits from UpdateAPIView provided by Django REST Framework.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def update(self, request, *args, **kwargs):
+        book = self.get_object()
+
+        # Check if the book is available
+        if not book.available:
+            return Response({"error": "This book is already borrowed"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Update book availability and last borrowed date  
+        book.available = False
+        book.last_borrowed_date = timezone.now()
+        book.save()
+
+        return Response({"message": "Book borrowed successfully"}, status=status.HTTP_200_OK)
