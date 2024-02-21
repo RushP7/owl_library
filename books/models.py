d49087067347834f4dc67b66919766ff74df816b
RushP7 <rushilxo5@gmail.com>
2024-02-21 15:45:49 -0500

    updated book model

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Book(models.Model):
     """
@@ -9,6 +10,8 @@ class Book(models.Model):
         author (str): The author of the book.
         owl_id (str): The unique ID of the book.
         book_type (str): The type of the book (e.g., 'PB' for Paperback).
+        available (bool): Whether the book is available to borrow.
+        last_borrowed (datetime): The date and time the book was last borrowed.
     """
 
     TYPE_CHOICES = [
@@ -20,6 +23,8 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     owl_id = models.CharField(max_length=255, unique=True)
     book_type = models.CharField(max_length=2, choices=TYPE_CHOICES)
-
+    available = models.BooleanField(default=True)
+    last_borrowed = models.DateTimeField(null=True)
+    
     def __str__(self):
         return str(self.title)
