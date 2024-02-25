d9cf207aa355615fcb2b5ce10c74aefbaa318216
RushP7 <rushilxo5@gmail.com>
2024-02-25 16:20:54 -0500

    user and borrowhistory models

--- books/models.py
+++ books/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.utils import timezone
+
+User = get_user_model()
 
 class Book(models.Model):
     """
@@ -14,17 +17,20 @@ class Book(models.Model):
         last_borrowed (datetime): The date and time the book was last borrowed.
     """
 
-    TYPE_CHOICES = [
-        ('PB', 'Paperback'),
-        ('HC', 'Hardcover'),
-        ('HM', 'Handmade'),
-    ]
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     owl_id = models.CharField(max_length=255, unique=True)
-    book_type = models.CharField(max_length=2, choices=TYPE_CHOICES)
+    book_type = models.CharField(max_length=2, choices=[('PB', 'Paperback'), ('HC', 'Hardcover'), ('HM', 'Handmade')])
     available = models.BooleanField(default=True)
-    last_borrowed = models.DateTimeField(null=True)
-    
+    last_borrowed = models.DateTimeField(null=True, blank=True)
+
     def __str__(self):
-        return str(self.title)
+        return f"{self.title} by {self.author}"
+
+class BorrowHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='borrow_history')
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    borrow_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} borrowed {self.book.title}"
